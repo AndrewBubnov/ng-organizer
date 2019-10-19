d0e5963df7a58b8022187bc8a3a7a9b822229f24
AndrewBubnov <andrew.bubnov75@gmail.com>
2019-10-19 12:05:33 +0300

    dynamic remove task added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SelectorComponent } from './components/selector/selector.component';
 import { MomentPipe } from './pipes/moment.pipe';
+import { EllipsisPipe } from './pipes/elipsify.pipe';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { OrganizerComponent } from './components/organizer/organizer.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { ErrorSnackbarComponent } from './components/error-snackbar/error-snackbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SelectorComponent,
     MomentPipe,
+    EllipsisPipe,
     CalendarComponent,
     OrganizerComponent,
     ErrorSnackbarComponent,
---
+++ src/app/pipes/elipsify.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+@Pipe({
+  name: 'ellipsify',
+  pure: false,
+})
+export class EllipsisPipe implements PipeTransform {
+  transform(str: string): string {
+    return str.length > 10 ? str.substring(0, 10) + '...' : str;
+  }
+}
