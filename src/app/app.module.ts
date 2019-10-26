1280add7d3f1db8916c50cd7b06879bf88efc657
AndrewBubnov <andrew.bubnov75@gmail.com>
2019-10-26 14:20:15 +0300

    error interceptor service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,6 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material';
 import { HAMMER_GESTURE_CONFIG, HammerGestureConfig } from '@angular/platform-browser';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import {InterceptorService} from './services/interceptor.service';
 
 export class MyHammerConfig extends HammerGestureConfig {
@@ -41,7 +39,6 @@ export class MyHammerConfig extends HammerGestureConfig {
     HttpClientModule,
     BrowserAnimationsModule,
     MatSnackBarModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     {
